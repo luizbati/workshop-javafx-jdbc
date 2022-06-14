8276cbefa6b7261a4fec42a4be5869ef843d54ec
Luiz <luizcb1944@hotmail.com>
2022-06-14 10:21:44 -0300

    Saving a new department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -6,15 +6,23 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
-	
+
 	public static Stage currentStage(ActionEvent event) {
-		return (Stage)((Node)event.getSource()).getScene().getWindow();
-		
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+
 	}
 
 	public static Stage currentStage(javafx.event.ActionEvent event) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+
+	}
 }
