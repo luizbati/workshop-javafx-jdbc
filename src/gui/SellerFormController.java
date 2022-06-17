5d12b7cb53d1a065b3bb29eb5e4469972e8d716a
Luiz <luizcb1944@hotmail.com>
2022-06-17 08:58:25 -0300

    Seller form

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,139 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exception.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+	
+	private Seller entity;
+	
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListener = new ArrayList<>();
+	
+
+	@FXML private TextField txtId;
+	@FXML private TextField txtName;
+	@FXML Label labelErrorName;
+	@FXML private Button btnSave;
+	@FXML private Button btnCancel;
+
+	private ResourceBundle errors;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService (SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListener.add(listener);
+	}
+	
+	
+	@FXML private void onBtnSaveAction(ActionEvent event) {
+		if(service == null){
+		throw new IllegalStateException("Service was null");
+		}
+		
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		
+		
+		try {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		notifyDataChangeListeners();
+		Utils.currentStage(event).close();
+			
+		}
+		catch(ValidationException e) {
+			setErrorMessage(e.getErrors());
+		}
+		catch
+			(DbException e){
+				Alerts.showAlert("Error saving object", null, e.getMessage(),AlertType.ERROR);
+			}
+			}
+	
+		private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListener) {
+			listener.ondataChanged();
+		}
+		
+	}
+
+		private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+		
+	}
+
+	@FXML public void onBtnCancelaction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormDate() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+
+
+	public void updateFormData() {
+		
+		
+	}
+	
+	private void setErrorMessage(Map<String,String>error) {
+		Set<String>fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.getString("name"));
+		}
+		
+	}
+
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.List;
@@ -15,7 +16,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -23,6 +26,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.entities.seller;
@@ -103,28 +108,28 @@ public class SellerListController implements Initializable, DataChangeListener {
 		initRemoveButtons();
 	}
 
-//	private void createDialogForm(seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//
-//			sellerFormController controller = loader.getController();
-//			controller.setseller(obj);
-//			controller.setsellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this);
-//			controller.updateFormData();
-//
-//			Stage dialogStage = new Stage();
-//			dialogStage.setTitle("Enter seller name data");
-//			dialogStage.setScene(new Scene(pane));
-//			dialogStage.setResizable(false);
-//			dialogStage.initOwner(parentStage);
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+	private void createDialogForm(seller obj, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			sellerFormController controller = loader.getController();
+			controller.setseller(obj);
+			controller.setsellerService(new SellerService());
+			controller.subscribeDataChangeListener(this);
+			controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter seller name data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
